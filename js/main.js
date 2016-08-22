c3315586beb46a24940b5eae2ee95215783f7627
Tanja Nikolic <tanja.nikolic.rs@gmail.com>
2016-08-23 01:26:32 +0200

    home page

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
+$(document).ready(function () {
 
+    $("article .dinnerHover").hover(function () {
+        $('.din-w').hide();
+        $('.din').show();
+    });
+    $("article .dinnerHover").mouseleave(function () {
+        $('.din-w').show();
+        $('.din').hide();
+    });
+
+
+
+
+
+
+
+});
