eaf713869c17089cfa0e96c9610c3ec83b37085d
Tanja Nikolic <tanja.nikolic.rs@gmail.com>
2016-08-27 19:03:49 +0200

    home page

--- js/main.js
+++ js/main.js
@@ -1,31 +1,67 @@
 $(document).ready(function () {
 
-    
+
 
 //Date & Time picker
-$('.form_date').datetimepicker({
-    
+    $('.form_date').datetimepicker({
         weekStart: 1,
-        todayBtn:  1,
-		autoclose: 1,
-		todayHighlight: 1,
-		startView: 2,
-		minView: 2,
-		forceParse: 0
+        todayBtn: 1,
+        autoclose: 1,
+        todayHighlight: 1,
+        startView: 2,
+        minView: 2,
+        forceParse: 0
     });
-	$('.form_time').datetimepicker({
-        language:  'fr',
+    $('.form_time').datetimepicker({
+        language: 'fr',
         weekStart: 1,
-        todayBtn:  1,
-		autoclose: 1,
-		todayHighlight: 1,
-		startView: 1,
-		minView: 0,
-		maxView: 1,
-		forceParse: 0
+        todayBtn: 1,
+        autoclose: 1,
+        todayHighlight: 1,
+        startView: 1,
+        minView: 0,
+        maxView: 1,
+        forceParse: 0
     });
 
 
+//Navigation change background on scroll
+    var didScroll;
+    var lastScrollTop = 50;
+    var delta = 5;
+    var navbarHeight = $('header').outerHeight();
+
+    $(window).scroll(function (event) {
+        didScroll = true;
+    });
+
+    setInterval(function () {
+        if (didScroll) {
+            hasScrolled();
+            didScroll = false;
+        }
+    }, 250);
+    function hasScrolled() {
+        var st = $(this).scrollTop();
+
+        if (Math.abs(lastScrollTop - st) <= delta)
+            return;
+
+        if (st > lastScrollTop && st > navbarHeight) {
+            // Scroll Down
+            $('header').removeClass('nav-down').addClass('nav-up');
+            $('.navbar-brand > img').removeClass('navImg-up').addClass('navImg-down');
+        } else {
+            // Scroll Up
+            if (st + $(window).height() < $(document).height()) {
+                $('header').removeClass('nav-up').addClass('nav-down');
+                $('.navbar-brand > img').removeClass('navImg-down').addClass('navImg-up');
+                
+            }
+        }
+
+        lastScrollTop = st;
+    }
 
 
 
